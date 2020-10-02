ed500154f6aefc0a6cd3823f55a09fb1458c6008
zonapplication <48274227+zonapplication@users.noreply.github.com>
2020-10-02 14:37:23 +0700

    Update PhotoLibraryPickerViewController.swift
    added default case

--- Source/Pickers/PhotoLibrary/PhotoLibraryPickerViewController.swift
+++ Source/Pickers/PhotoLibrary/PhotoLibraryPickerViewController.swift
@@ -188,6 +188,8 @@ final public class PhotoLibraryPickerViewController: UIViewController {
                 self.alertController?.dismiss(animated: true)
             }
             alert.show()
+        default:
+            break
         }
     }
     
